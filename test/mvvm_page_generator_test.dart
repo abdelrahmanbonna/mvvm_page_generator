02291cea582ff28962166ae16c9944f5dc01fadc
Abdelrahman.elkotb <abdelrahman.elkotb@etisalat.com>
2022-03-21 16:45:56 +0200

    adding generators unit testing

--- test/mvvm_page_generator_test.dart
+++ test/mvvm_page_generator_test.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:mvvm_page_generator/mvvm_page_generator.dart';
 import 'package:test/test.dart';
 
@@ -8,12 +9,23 @@ void main() {
     test('Testing if verifyClassName function works fine', () {
       assert(obj.verifyClassName() == true);
     });
+
+    test('Testing if verifyPath function works fine', () {
+      assert(obj.verifyPath() == true);
+    });
   });
 
   group('Generators group of tests', () {
-    test('Testing if files generates', () {
-      //TODO test generators here
-      
+    var obj = MVVMPageGenerator(className: 'home', path: 'lib');
+    obj.generatePages();
+    test('Testing if view is created', () async {
+      assert(await File('lib/Home/home_view.dart').exists());
+    });
+    test('Testing if viewModel is created', () async {
+      assert(await File('lib/Home/home_viewmodel.dart').exists());
+    });
+    test('Testing if service is created', () async {
+      assert(await File('lib/Home/home_service.dart').exists());
     });
   });
 }
